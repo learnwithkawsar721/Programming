efa32c7d38df8e4da8fab396b694910178cf969d
learnwithkawsar721 <kawsar721g@gmail.com>
2022-06-28 21:14:01 +0600

    primality text

---
+++ Number Theory/Primality Test/primality.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+bool prime(int n)
+{
+    if(n<2)return false;
+    if(n<=3)return true;
+    if(n%2==0)return false;
+    for(int i=3; i*i<=n; i+=2){
+        if(n%i==0)return false;
+    }
+    return true;
+}
+int main()
+{
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        if(prime(n))cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+    }
+}
